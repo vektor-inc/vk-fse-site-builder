bea7ca7ae00c1995acae0b6ad33081413d411b0a
kurudrive <kurudrive@gmail.com>
2022-01-13 23:41:15 +0900

    first commit

---
+++ vk-fse-site-builder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     VK FSE Site Builder
+ * Plugin URI:      https://github.com/vektor-inc/vk-fse-site-builder
+ * Description:     
+ * Author:          Vektor,Inc.
+ * Author URI:      https://vektor-inc.co.jp
+ * Text Domain:     vk-fse-site-builder
+ * Domain Path:     /languages
+ * Version:         0.0.0
+ *
+ * @package         vektor-inc/vk-fse-site-builder
+ */
+
+// Your code starts here.
