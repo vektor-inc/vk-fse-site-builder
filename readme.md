e320220d37035cbf7d771893cedd330a75c9e4a3
kurudrive <kurudrive@gmail.com>
2022-01-16 01:50:09 +0900

    ヘッダーのパターンとCSS初期設定を雑に追加

---
+++ readme.md
@@ -0,0 +1,5 @@
+## ブレイクポイント
+
+スマホ : `@media (max-width: 575.98px)`
+タブレット : `@media (min-width: 576px) and (max-width: 991.98px)`
+PC : `@media (min-width: 992px)`
--- vk-fse-site-builder.php
+++ vk-fse-site-builder.php
@@ -12,4 +12,28 @@
  * @package         vektor-inc/vk-fse-site-builder
  */
 
+ // Get Plugin version.
+$version = get_file_data( __FILE__, array( 'version' => 'Version' ) );
+define( 'VKSB_VERSION', $version['version'] );
+define( 'VKSB_DIRECTORY_PATH', dirname( __FILE__ ) );
+define( 'VKSB_DIRECTORY_URL', plugins_url( '', __FILE__ ) );
+
 // Your code starts here.
+include( dirname( __FILE__ ) . '/patterns-data/class-register-patterns-from-json.php' );
+
+function vksb_support() {
+
+	// Enqueue editor styles.
+	// add_editor_style( 'assets/css/style.css' );
+	// add_editor_style( 'assets/css/editor-style.css' );
+}
+add_action( 'after_setup_theme', 'vksb_support' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function vksb_scripts() {
+	// Enqueue theme stylesheet.
+	wp_enqueue_style( 'vksb-style', VKSB_DIRECTORY_URL . '/assets/css/style.css', array(), VKSB_VERSION );
+}
+add_action( 'wp_enqueue_scripts', 'vksb_scripts' );
